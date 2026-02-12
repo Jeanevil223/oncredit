03d8e6e3f5da488274e1ea0359b1c60e9b586b5c
Jeanevil223 <jeanccfernandes090@gmail.com>
2026-02-12 11:16:11 -0300

    send

--- lib/services/client_service.dart
+++ lib/services/client_service.dart
@@ -2,20 +2,17 @@
 
 import 'package:dio/dio.dart';
 import '../config/app_config.dart';
-import '../models/client.dart';
+import '../models/clients.dart';
 
 class ClientService {
   final Dio _dio = Dio();
 
   Future<List<Client>> getClients() async {
     final uid = AppConfig.fixedUid;
-
     final response = await _dio.get(
       '${AppConfig.baseUrl}/users/$uid/clients.json',
     );
-
     final data = response.data as Map<String, dynamic>? ?? {};
-
     return data.entries.map((e) => Client.fromMap(e.key, e.value)).toList();
   }
 
@@ -25,7 +22,6 @@ class ClientService {
     required List<String> phones,
   }) async {
     final uid = AppConfig.fixedUid;
-
     final response = await _dio.post(
       '${AppConfig.baseUrl}/users/$uid/clients.json',
       data: {
@@ -35,21 +31,33 @@ class ClientService {
         'createdAt': DateTime.now().toIso8601String().substring(0, 10),
       },
     );
-
     final generatedId = response.data['name'];
-
     return Client(id: generatedId, name: name, cpf: cpf);
   }
 
   Future<bool> cpfExists(String cpf) async {
     final uid = AppConfig.fixedUid;
-
     final response = await _dio.get(
       '${AppConfig.baseUrl}/users/$uid/clients.json',
     );
-
     final data = response.data as Map<String, dynamic>? ?? {};
-
     return data.values.any((c) => c['cpf'] == cpf);
+  }
+
+  Future<void> updateClient({
+    required String id,
+    required String name,
+    required List<String> phones,
+  }) async {
+    final uid = AppConfig.fixedUid;
+    await _dio.patch(
+      '${AppConfig.baseUrl}/users/$uid/clients/$id.json',
+      data: {'name': name, 'phones': phones},
+    );
+  }
+
+  Future<void> deleteClient(String clientId) async {
+    final uid = AppConfig.fixedUid;
+    await _dio.delete('${AppConfig.baseUrl}/users/$uid/clients/$clientId.json');
   }
 }
--- lib/services/finance_service.dart
+++ lib/services/finance_service.dart
@@ -2,6 +2,7 @@
 
 import 'package:dio/dio.dart';
 import '../config/app_config.dart';
+import '../models/financial_event.dart';
 
 class FinanceService {
   final Dio _dio = Dio();
@@ -68,5 +69,73 @@ class FinanceService {
       'payments': totalPayments,
       'balance': totalPurchases - totalPayments,
     };
+  }
+
+  Future<List<FinancialEvent>> getClientHistory(String clientId) async {
+    final uid = AppConfig.fixedUid;
+
+    final purchasesResponse = await _dio.get(
+      '${AppConfig.baseUrl}/users/$uid/purchases.json',
+    );
+
+    final paymentsResponse = await _dio.get(
+      '${AppConfig.baseUrl}/users/$uid/payments.json',
+    );
+
+    final List<FinancialEvent> history = [];
+
+    final purchases = purchasesResponse.data as Map<String, dynamic>? ?? {};
+    final payments = paymentsResponse.data as Map<String, dynamic>? ?? {};
+
+    for (final p in purchases.values) {
+      if (p['clientId'] == clientId) {
+        history.add(
+          FinancialEvent(
+            description: 'Compra',
+            value: (p['totalValue'] as num).toDouble(),
+            date: DateTime.parse(p['date']),
+            type: FinancialEventType.purchase,
+          ),
+        );
+      }
+    }
+
+    for (final p in payments.values) {
+      if (p['clientId'] == clientId) {
+        history.add(
+          FinancialEvent(
+            description: 'Pagamento',
+            value: (p['value'] as num).toDouble(),
+            date: DateTime.parse(p['date']),
+            type: FinancialEventType.payment,
+          ),
+        );
+      }
+    }
+
+    history.sort((a, b) => b.date.compareTo(a.date));
+
+    return history;
+  }
+
+  Future<void> deleteClientHistory(String clientId) async {
+    final uid = AppConfig.fixedUid;
+
+    Future<void> deleteByClient(String path) async {
+      final res = await _dio.get('${AppConfig.baseUrl}/users/$uid/$path.json');
+
+      final data = res.data as Map<String, dynamic>? ?? {};
+
+      for (final entry in data.entries) {
+        if (entry.value['clientId'] == clientId) {
+          await _dio.delete(
+            '${AppConfig.baseUrl}/users/$uid/$path/${entry.key}.json',
+          );
+        }
+      }
+    }
+
+    await deleteByClient('purchases');
+    await deleteByClient('payments');
   }
 }
