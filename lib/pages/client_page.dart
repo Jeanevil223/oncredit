23e08855a34f808deea1a5de180a8127bbac9af4
Jeanevil223 <jeanccfernandes090@gmail.com>
2026-02-19 08:52:57 -0300

    send

--- lib/pages/client_page.dart
+++ lib/pages/client_page.dart
@@ -1,7 +1,7 @@
 // lib/pages/client_page.dart
 
 import 'package:flutter/material.dart';
-import '../models/clients.dart';
+import '../models/client.dart';
 import '../services/client_service.dart';
 import '../templates/appbar.dart';
 import '../services/finance_service.dart';
@@ -10,6 +10,9 @@ import 'client_edit_page.dart';
 import 'client_history_page.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import 'new_payment_page.dart';
+import 'new_purchase_page.dart';
 
 String onlyNumbers(String value) {
   return value.replaceAll(RegExp(r'[^0-9]'), '');
@@ -57,12 +60,35 @@ class _ClientPageState extends State<ClientPage> {
             ListTile(
               leading: const Icon(Icons.shopping_cart),
               title: const Text('Registrar compra'),
-              onTap: () {},
+              onTap: () async {
+                final result = await Navigator.push<bool>(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => NewPurchasePage(clientId: client.id),
+                  ),
+                );
+
+                if (result == true) {
+                  setState(() {}); // recarrega resumo financeiro
+                }
+              },
             ),
+
             ListTile(
               leading: const Icon(Icons.payments),
               title: const Text('Registrar pagamento'),
-              onTap: () {},
+              onTap: () async {
+                final result = await Navigator.push<bool>(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => NewPaymentPage(clientId: client.id),
+                  ),
+                );
+
+                if (result == true) {
+                  setState(() {}); // atualiza resumo financeiro
+                }
+              },
             ),
 
             const SizedBox(height: 16),
